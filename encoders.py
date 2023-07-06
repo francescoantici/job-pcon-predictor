2db9e5edc8c9dc406b207fdbf8aca30752807cde
francescoantici <francesco.antici98@gmail.com>
2023-07-06 16:31:51 +0900

    encoders update

--- encoders.py
+++ encoders.py
@@ -2,16 +2,19 @@ import numpy as np
 import pandas as pd
 from sentence_transformers import SentenceTransformer
 
-JOB_FEATURES = ["usr", "jnam", "cnumr", "nnumr", "CR-STR-jobenv-req"]
+JOB_FEATURES = ["usr", "jnam", "cnumr", "nnumr", "jobenv_req"]
 
 def sb_encoding_function(df):
     
+    # Pull the SBert model
     encoder = SentenceTransformer("all-MiniLM-L6-v2")
     
     x_str = np.zeros((len(df), 384))
     
+    # Convert the job data to a comma divided string
     convert_to_str = lambda job: ",".join([f"{job[k]}" for k in job.index if (job[k] and not(pd.isna(job[k])))])
     
+    # Encode the jobs
     encoded_str = encoder.encode(df[JOB_FEATURES].apply(convert_to_str, axis = 1).values)
     
     for i in range(len(df)):
@@ -23,9 +26,8 @@ def int_encoding_function(df):
     
     JOB_INT_FEATURES = JOB_FEATURES.copy()
     
-    for feat in ["usr", "grp", "jtyp", "jnam", "hostname", "CR-STR-jobenv-req"]:
-        if feat not in JOB_FEATURES:
-            continue
+    # Encode to categorical the textual features
+    for feat in ["usr", "jnam", "hostname", "jobenv_req"]:
         JOB_INT_FEATURES.remove(feat)
         cat_feat = f"{feat}_cat"
         df.loc[:, cat_feat] = pd.Categorical(df.loc[:, feat]).codes
