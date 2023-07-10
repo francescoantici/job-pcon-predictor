1fdb118b737afb7d8bbf0827a07bb10c03b1f3ac
francescoantici <francesco.antici98@gmail.com>
2023-07-10 16:15:32 +0900

    Update encoders.py

--- encoders.py
+++ encoders.py
@@ -4,25 +4,25 @@ from sentence_transformers import SentenceTransformer
 
 JOB_FEATURES = ["usr", "jnam", "cnumr", "nnumr", "jobenv_req"]
 
-def sb_encoding_function(df):
+def sb_encoding_function(jobs_df:pd.DataFrame) -> np.array:
     
     # Pull the SBert model
     encoder = SentenceTransformer("all-MiniLM-L6-v2")
     
-    x_str = np.zeros((len(df), 384))
+    x_str = np.zeros((len(jobs_df), 384))
     
     # Convert the job data to a comma divided string
     convert_to_str = lambda job: ",".join([f"{job[k]}" for k in job.index if (job[k] and not(pd.isna(job[k])))])
     
     # Encode the jobs
-    encoded_str = encoder.encode(df[JOB_FEATURES].apply(convert_to_str, axis = 1).values)
+    encoded_str = encoder.encode(jobs_df[JOB_FEATURES].apply(convert_to_str, axis = 1).values)
     
-    for i in range(len(df)):
+    for i in range(len(jobs_df)):
         x_str[i] = encoded_str[i]
     
     return x_str
 
-def int_encoding_function(df):
+def int_encoding_function(jobs_df: pd.DataFrame) -> np.array:
     
     # Create a different set of features' name
     JOB_INT_FEATURES = JOB_FEATURES.copy()
@@ -31,7 +31,7 @@ def int_encoding_function(df):
     for feat in ["usr", "jnam", "hostname", "jobenv_req"]:
         JOB_INT_FEATURES.remove(feat)
         cat_feat = f"{feat}_cat"
-        df.loc[:, cat_feat] = pd.Categorical(df.loc[:, feat]).codes
+        jobs_df.loc[:, cat_feat] = pd.Categorical(jobs_df.loc[:, feat]).codes
         JOB_INT_FEATURES.append(cat_feat)
     
-    return df[JOB_INT_FEATURES].values
+    return jobs_df[JOB_INT_FEATURES].values
