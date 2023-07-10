2bfab71fa847ee345dc0e9fb61aa88d2e5099920
francescoantici <francesco.antici98@gmail.com>
2023-07-10 12:13:25 +0900

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -11,12 +11,16 @@ if __name__ == "__main__":
     
     job_df_path = ""
     
+    # Order the jobs by submission time to recreate the original timeline
     job_df = pd.read_csv(job_df_path).sort_values("submit_time")
     
+    # Normalize the Maximum and Average power consumption values by node number
     job_df["nmaxpcon"] = job_df.apply(lambda job: int(int(job.maxpcon)/int(job.nnuma))).values 
     job_df["navgpcon"] = job_df.ppr.apply(lambda job: int(int(job.avgpcon)/int(job.nnuma))).values
     
+    # Each entry of the dict is an experimental setup for a model, the target represent the task (MAX or AVG)
     models_setup_dict = {
+        # MAX task models' setup
         "SB+RF_MAX": 
             {
                 "target_feat" : "nmaxpcon",
@@ -85,6 +89,7 @@ if __name__ == "__main__":
                     "random_state" : RANDOM_SEED,
                 }
             },
+        # AVG task models' setup
         "SB+RF_AVG": 
             {
                 "target_feat" : "navgpcon",
@@ -155,5 +160,6 @@ if __name__ == "__main__":
             },
     }
     
+    # Launch of the online experiments
     online_experiment(jobs_df = job_df, models_setup_dict = models_setup_dict)
     
