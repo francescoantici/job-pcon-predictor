245d0401a5b89603196b1b9fdb704c11f586bee5
francescoantici <francesco.antici98@gmail.com>
2023-07-10 12:13:46 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,18 @@
+from typing import Iterable
 from sklearn.metrics import mean_absolute_error, mean_absolute_percentage_error, mean_squared_error, r2_score
 
-def regression_report(y_true, y_pred, digits:int = 2) -> str:
+def regression_report(y_true:Iterable, y_pred:Iterable, digits:int = 2) -> str:
+    """_summary_
+
+    Args:
+        y_true (Iterable): The true elements
+        y_pred (Iterable): The predicted elements
+        digits (int, optional): Number of digits to show in the report values. Defaults to 2.
+
+    Returns:
+        str: The regression report
+    """
+    assert len(y_true) == len(y_pred), print(f"Lenghts of arrays don't match, got {len(y_pred)} and {len(y_true)}")
     headers = ["mae", "mape", "mse", "r2", "support"]
     mae = mean_absolute_error(y_true, y_pred)
     mape = mean_absolute_percentage_error(y_true, y_pred)
